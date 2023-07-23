f9732fa8520d5d7589077df2372d5f0275e5978c
arroyostack <arroyostack@gmail.com>
2023-07-24 01:10:50 +0200

    Feat reset addContact form

--- src/components/NewContactForm.jsx
+++ src/components/NewContactForm.jsx
@@ -14,6 +14,7 @@ export const NewContactForm = () => {
     const [ number, setNumber ] = useState("");
     const [ image, setImage ] = useState("");
     const [ company, setCompany ] = useState("");
+
     const newUSer = {
         name,
         email,
@@ -28,12 +29,24 @@ export const NewContactForm = () => {
         setRedirect(false);
         e.preventDefault();
         if(name.length < 1) {
+            //Note: Add more validations.
             alert("name must be at least two ");
+            return;
         }
         const data = await addContact(newUSer, uuid());
         createContact(contacts);
         setRedirect(true);
 
+    };
+
+    // RESET FORM VARIABLES STATE
+    const handleReset = () => {
+        setName("");
+        setEmail("");
+        setAddress("");
+        setNumber("");
+        setImage("");
+        setCompany("");
     };
 
 
@@ -63,8 +76,8 @@ export const NewContactForm = () => {
             { redirect && <Navigate replace to="/" /> }
             <form className='container bg-light p-5 mt-5 rounded' onSubmit={ handleSubmit }>
                 <div className="input-group mb-3 row">
-                    <label className="col-form-label" htmlFor=""><h6>Name</h6></label>
-                    <input
+                    <label className="col-form-label" htmlFor="name"><h6>Name</h6></label>
+                    <input id="name"
                         type="text"
                         value={ name }
                         onChange={ onNameChange }
@@ -73,7 +86,7 @@ export const NewContactForm = () => {
                 </div>
                 <div className="input-group mb-3 row">
                     <label className="col-form-label" htmlFor=""><h6>Email</h6></label>
-                    <input
+                    <input id="email"
                         type="email"
                         value={ email }
                         onChange={ onEmailChange }
@@ -81,8 +94,8 @@ export const NewContactForm = () => {
                     />
                 </div>
                 <div className="input-group mb-3 row">
-                    <label className="col-form-label" htmlFor=""><h6>Address</h6></label>
-                    <input
+                    <label className="col-form-label" htmlFor="address"><h6>Address</h6></label>
+                    <input id="address"
                         type="text"
                         value={ address }
                         onChange={ onAddressChange }
@@ -90,8 +103,8 @@ export const NewContactForm = () => {
                     />
                 </div>
                 <div className="input-group mb-3 row">
-                    <label className="col-form-label" htmlFor=""><h6>Phone Number</h6></label>
-                    <input
+                    <label className="col-form-label" htmlFor="phone-number"><h6>Phone Number</h6></label>
+                    <input id="phone-number"
                         type="tel"
                         value={ number }
                         onChange={ onNumberChange }
@@ -99,8 +112,8 @@ export const NewContactForm = () => {
                     />
                 </div>
                 <div className="input-group mb-3 row">
-                    <label className="col-form-label" htmlFor=""><h6>Image</h6></label>
-                    <input
+                    <label className="col-form-label" htmlFor="Image"><h6>Image</h6></label>
+                    <input id="Image"
                         type="url"
                         value={ image }
                         onChange={ onImageChange }
@@ -108,18 +121,18 @@ export const NewContactForm = () => {
                     />
                 </div>
                 <div className="input-group mb-3 row">
-                    <label className="col-form-label" htmlFor=""><h6>Company</h6></label>
-                    <input
+                    <label className="col-form-label" htmlFor="company"><h6>Company</h6></label>
+                    <input id="company"
                         type="text"
                         value={ company }
                         onChange={ onCompanyChange }
                         className="form-control rounded w-50"
                     />
                 </div>
-                <div className="d-grid gap-2">
-
+                <div className="">
+                    <button type="submit" className="btn btn-primary me-3">Button</button>
+                    <button type="reset" className="btn btn-primary me-3" onClick={ handleReset }>Reset</button>
                 </div>
-                <button type="submit" className="btn btn-primary">Button</button>
             </form>
         </>
     );
